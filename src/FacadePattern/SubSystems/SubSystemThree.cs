04053452ebbe359ce14745718429bbc23c7f4d9d
沈平元 <2439739932@qq.com>
2021-12-06 19:19:29 +0800

    add facade pattern

---
+++ src/FacadePattern/SubSystems/SubSystemThree.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadePattern.SubSystems
+{
+    public class SubSystemThree
+    {
+        public void MethodThree()
+        {
+            Console.WriteLine("subsystem method three");
+        }
+    }
+}
