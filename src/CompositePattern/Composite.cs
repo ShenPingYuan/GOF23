f5d08fdcd8ccd742f3437c2bf2e5789e7cdf92b3
MichaelShen_company <2439739932@qq.com>
2021-12-09 15:07:22 +0800

    add component pattern

---
+++ src/CompositePattern/Composite.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CompositePattern
+{
+    public class Composite:Component
+    {
+        private List<Component> children = new List<Component>();
+
+        public Composite(string name) : base(name)
+        {
+        }
+
+        public override void Add(Component component)
+        {
+            children.Add(component);
+        }
+
+        public override void Display(int depth)
+        {
+            Console.WriteLine(new String('-',depth)+name);
+            foreach (Component component in children)
+            {
+                component.Display(depth + 2);
+            }
+        }
+
+        public override void Remove(Component component)
+        {
+            children.Remove(component);
+        }
+    }
+}
---
+++ src/CompositePattern/Leaf.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositePattern
+{
+    public class Leaf : Component
+    {
+        public Leaf(string name) : base(name)
+        {
+        }
+
+        public override void Add(Component component)
+        {
+            Console.WriteLine("cannot add to a leaf");
+        }
+
+        public override void Display(int depth)
+        {
+            Console.WriteLine(new string('-', depth) + name);
+        }
+
+        public override void Remove(Component component)
+        {
+            Console.WriteLine("cannot remove from a leaf");
+        }
+    }
+}
