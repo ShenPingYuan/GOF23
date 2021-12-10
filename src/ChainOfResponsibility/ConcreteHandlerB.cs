70b4e253aca6c868b858288cdac344f55e88923b
MichaelShen_company <2439739932@qq.com>
2021-12-10 17:46:31 +0800

    add chain Of Responsibility pattern

---
+++ src/ChainOfResponsibility/ConcreteHandlerB.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    public class ConcreteHandlerB : Handler
+    {
+        public override void HandleRequest(int request)
+        {
+            if (request >= 10 && request < 20)
+            {
+                Console.WriteLine($"{this.GetType().Name} 处理了请求：{request}");
+            }
+            else
+            {
+                if (successor != null)
+                {
+                    successor.HandleRequest(request);
+                }
+            }
+        }
+    }
+}
---
+++ src/ChainOfResponsibility/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ChainOfResponsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Handler handlerA = new ConcreteHandlerA();
+            Handler handlerB = new ConcreteHandlerB();
+            Handler handlerC = new ConcreteHandlerC();
+
+            handlerA.SetSuccessor(handlerB);
+            handlerB.SetSuccessor(handlerC);
+
+            int[] requests = new int[] { 1, 2, 4, 6, 7, 8, 12, 12, 14, 22, 4, 25, 46, 34, 45 };
+            foreach (int request in requests)
+            {
+                handlerA.HandleRequest(request);
+            }
+        }
+    }
+}
