4e1859578995d3650e0d83894982526254342cfc
沈平元 <2439739932@qq.com>
2021-12-11 16:56:00 +0800

    add flyweight pattern

---
+++ src/FlyweightPattern/FlyweightFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlyweightPattern
+{
+    public class FlyweightFactory
+    {
+        private Hashtable flyweights = new Hashtable();
+        public FlyweightFactory()
+        {
+            flyweights.Add("X", new ConcreteFlyweight());
+            flyweights.Add("Y", new ConcreteFlyweight());
+            flyweights.Add("Z", new ConcreteFlyweight());
+        }
+        public IFlyweight GetFlyweight(string key)
+        {
+            return (IFlyweight)flyweights[key];
+        }
+    }
+}
---
+++ src/FlyweightPattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FlyweightPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int extrinsicstate = 22;
+            FlyweightFactory factory = new FlyweightFactory();
+
+            IFlyweight fx = factory.GetFlyweight("X");
+            fx.Operation(--extrinsicstate);
+
+            IFlyweight fy = factory.GetFlyweight("Y");
+            fx.Operation(--extrinsicstate);
+
+            IFlyweight fz = factory.GetFlyweight("Z");
+            fx.Operation(--extrinsicstate);
+
+            UnsharedConcreteFlyweight uf=new UnsharedConcreteFlyweight();
+            uf.Operation(--extrinsicstate);
+        }
+    }
+}
