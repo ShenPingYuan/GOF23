b1fe9bb3cbd92c8234cba3ad27f2fa3ada43f93a
沈平元 <2439739932@qq.com>
2021-12-12 21:21:31 +0800

    add visitor pattern

---
+++ src/VisitorPattern/ConcreteElementA.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorPattern
+{
+    public class ConcreteElementA : Element
+    {
+        //双分派技术
+        public override void Accept(Visitor visitor)
+        {
+            visitor.VisitConcreteElementA(this);
+        }
+        public void OperationA() { }
+    }
+}
---
+++ src/VisitorPattern/ConcreteVisitor1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorPattern
+{
+    public class ConcreteVisitor1 : Visitor
+    {
+        public override void VisitConcreteElementA(ConcreteElementA concreteElementA)
+        {
+            Console.WriteLine($"{concreteElementA.GetType().Name}被{this.GetType().Name}访问");
+        }
+
+        public override void VisitConcreteElementB(ConcreteElementB concreteElementB)
+        {
+            Console.WriteLine($"{concreteElementB.GetType().Name}被{this.GetType().Name}访问");
+        }
+    }
+}
